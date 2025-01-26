fd5b4bb245cfe8f9afb05e816e12585809e51594
Asma Rana <95840056+asmarana@users.noreply.github.com>
2025-01-27 02:32:55 +0500

    fixed error

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 const API_URL =
-  typeof window !== 'undefined'
-    ? `${process.env.NEXT_PUBLIC_API_URL}/api/questions`
-    : 'http://localhost:3000/api/questions';
+  typeof window === 'undefined'
+    ? 'http://localhost:3000/api/questions'
+    : `${process.env.NEXT_PUBLIC_API_URL}/api/questions`;
 
 export { API_URL };
